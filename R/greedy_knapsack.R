25382c3b4e67c316992af97efa62477c9f743514
DucDuong92 <minhducduong92@gmail.com>
2018-10-10 14:21:08 +0200

    Update R/greedy_knapsack.R

---
+++ R/greedy_knapsack.R
@@ -0,0 +1,48 @@
+#' @title
+#' greedy_knapsack
+#' @description greedy_knapsack function
+#' @name
+#' greedy_knapsack
+#'
+#' @param x , is the input 
+#' @param W , is the 
+#'
+#'
+#' @return list , returns a list
+#' @export
+
+greedy_knapsack <- function(x,W)
+{ 
+  #value per weight
+  vpw <- x[,2]/x[,1] 
+  x$vpw <- vpw
+  #order
+  x <- x[order(x$vpw, decreasing = TRUE),]
+  weight <- W
+  sum <- 0
+  i <-1
+  elements <- c()
+  nrow <- nrow(x)
+  #sum value until reach the weight
+  for (i in 1:nrow) 
+  {
+    if (x$w[i] < weight)
+      {
+    if (weight > 0) 
+        {
+        weight <- weight - x$w[i]
+        sum <- sum + x$v[i]
+        elements <- c(elements, as.numeric(row.names(x)[i]))
+        }     
+      } else  break()
+  }
+  
+  output <- list(value = round(sum, 0), elements = elements)
+return(output)
+}
+
+#greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+#greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
+
+
+
