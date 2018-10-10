faa7aed496f5ad0b7509b1c37ff1dec5dff009b2
tejashreerm <43232587+tejashreerm@users.noreply.github.com>
2018-10-10 17:57:58 +0530

    Brute_force

---
+++ R/Brute_force_knapsack.R
@@ -0,0 +1,12 @@
+#' @title
+#' brute_force_knapsack
+#' @description Brute_force_knapsack function to solve the knapsack problem
+#' @name
+#' Brute_force_knapsack
+#'
+#' @param x , is the input weight
+#' @param W , is the input value
+#'
+#'
+#' @return list , returns a list that contain the maximum value and the list of elements chosen
+#' @export
