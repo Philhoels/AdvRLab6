4064844a394d671844c30d0a1e894129d09b8102
tejashreerm <43232587+tejashreerm@users.noreply.github.com>
2018-10-14 15:44:37 +0530

    Update Brute Force

--- R/Brute force.R
+++ R/Brute force.R
@@ -1,10 +1,12 @@
-set.seed(42)
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
+#'@title Brute Force Knapsack Algorithm
+#'@description Knapsack problem solution using Brute Force Algorithm in R.
+#'             It calculates best values and weights for given capacity.
+#'@param x Dataframe with variables v and w.
+#'@param W Integer as capacity.
+#'@param parallel Boolean makes function parallel
+#'@importFrom utils combn
+#'@export
+#'@return Returns a list with best value and combination.
 
 
 brute_force_knapsack <- function(x,W){
@@ -26,7 +28,7 @@ brute_force_knapsack <- function(x,W){
 
   Combinations_vector <- unlist(Combinations) # creating vectors by unlisting
   Weights_vector <- unlist(Weights)
-  Values_vector <- round(unlist(Values),0)
+  Values_vector <- round(unlist(Values))
 
   Capacity_weight <- which(Weights_vector < W) #using which to get the indexes of weight
   validValues <- Values_vector[Capacity_weight]
@@ -40,7 +42,7 @@ brute_force_knapsack <- function(x,W){
 }
 
 
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
-brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
-brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
+# brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+# brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
+# brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
+# brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
