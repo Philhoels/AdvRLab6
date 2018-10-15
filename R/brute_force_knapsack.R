403162c31b2b39eee385b3c773879fe837f50f22
tejashreerm <43232587+tejashreerm@users.noreply.github.com>
2018-10-16 00:59:13 +0530

    Update Brute force

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -4,7 +4,6 @@
 #'@param x Dataframe with variables v and w.
 #'@param W Integer as capacity.
 #'@param parallel Boolean makes function parallel
-#'@import lineprof parallel
 #'@importFrom utils combn
 #'@export
 #'@return Returns a list with best value and combination.
@@ -22,7 +21,7 @@ knapsack_objects <-data.frame(
 brute_force_knapsack <- function(x,W, parallel = TRUE){
 
   if(!is.data.frame(x) || W < 1){
-    stop("The argument W needs to be positive")
+    stop("functions rejects errounous input")
   }
   if(parallel == FALSE){
   Combinations <- list() #creating a list of combnations
@@ -42,7 +41,7 @@ brute_force_knapsack <- function(x,W, parallel = TRUE){
 
   Combinations_vector <- unlist(Combinations) # creating vectors by unlisting
   Weights_vector <- unlist(Weights)
-  Values_vector <- round(unlist(Values))
+  Values_vector <- round(unlist(Values),0)
 
   Capacity_weight <- which(Weights_vector < W) #using which to get the indexes of weight
   validValues <- Values_vector[Capacity_weight]
@@ -51,11 +50,11 @@ brute_force_knapsack <- function(x,W, parallel = TRUE){
   Value_Combinations <- which(Values_vector == maxValue) # using which to get the maximum Value
   validCombination <- Combinations_vector[Value_Combinations]
 
-  best_combination <- list(value = maxValue, elements = validCombination) #creating a list
+  best_combination <- list(value = maxValue, elements = as.numeric(strsplit(validCombination, " ")[[1]])) #creating a list
   }
    else{
-   cores <- parallel::detectCores()  #check the number of cores on the computer
-   cluster <- makeCluster(cores, type = "PSOCK") #to set up the cluster
+   cores <- parallel::detectCores() - 1  #check the number of cores on the computer
+   cluster <- makeCluster(cores) #to set up the cluster
    clusterExport(cluster, c("x"), envir = environment())
 
     Combinations <- parLapplyLB(cluster, 1:nrow(x), fun =  function(y) {
@@ -68,7 +67,7 @@ brute_force_knapsack <- function(x,W, parallel = TRUE){
              combn(x$v , y , sum)
 
     })
-    stopCluster(cluster) #shutdown cluster
+    stopCluster(cluster) #to shutdown cluster
     Combinations_vector <- unlist(Combinations) # creating vectors by unlisting
     Weights_vector <- unlist(Weights)
     Values_vector <- round(unlist(Values))
@@ -80,22 +79,13 @@ brute_force_knapsack <- function(x,W, parallel = TRUE){
     Value_Combinations <- which(Values_vector == maxValue) # using which to get the maximum Value
     validCombination <- Combinations_vector[Value_Combinations]
 
-    best_combination <- list(value = maxValue, elements = validCombination) #creating a list
+    best_combination <- list(value = maxValue, elements = as.numeric(strsplit(validCombination, " ")[[1]])) #creating a list
     }
     return(best_combination)
 }
 
-system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel = TRUE))
-system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
-system.time(brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500, parallel = TRUE))
-system.time(brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500))
-system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000, parallel = TRUE))
-system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000))
-system.time(brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000, parallel = TRUE))
-system.time(brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000))
+# brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel = TRUE)
+# system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel = TRUE))
 
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500, parallel = TRUE)
-brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500, parallel = TRUE)
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000, parallel = TRUE)
-brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000, parallel = TRUE)
+
 
